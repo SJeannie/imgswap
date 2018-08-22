0ba074087650463f43381b98616ae0c27c00027a
Kelly <devkells00@gmail.com>
2018-08-22 16:38:41 -0500

    Added Users show & edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :pictures 
     has_many :comments 
     has_secure_password
+    #validates :name, presence: true
+    #validates :email, presence: true
 end
